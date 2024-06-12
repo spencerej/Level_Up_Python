3a48ae910c3d25cdbb0c6b73442d5d714cf2a2d1
Eric Spencer <55814784+spencerej@users.noreply.github.com>
2024-06-11 20:18:00 -0700

    Add files via upload

---
+++ listitems.py
@@ -0,0 +1,10 @@
+def index_all(search_list, item):
+    index_list = []
+    for index, value in enumerate(search_list):
+        if value == item:
+            index_list.append([index])
+        elif isinstance(search_list[index], list):
+            for i in index_all(search_list[index], item):
+                index_list.append([index] + i)
+    return index_list
+
